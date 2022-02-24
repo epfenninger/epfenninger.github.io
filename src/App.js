9710cbc07c982f6050366607b7565132b4aa70c8
Ethan Pfenninger <epfenni@gmail.com>
2022-02-24 14:37:57 -0500

    routing working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
 import Contact from "./components/BasicContact.jsx";
+import NavBar from "./components/navbar";
+import HiddenFields from "./components/HiddenFields.jsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Contact />
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Contact />} />
+            <Route path="/HiddenFields" element={<HiddenFields />} />
+          </Routes>
+        </Router>
       </header>
     </div>
   );
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,53 +1,31 @@
-mport * as React from "react";
+import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import { Link, animateScroll } from "react-scroll";
-import Button from "@mui/material/Button";
-import Stack from "@mui/material/Stack";
+import { BrowserRouter, Link } from "react-router-dom";
 
-const buttonStyle = {
-  color: "white",
-  fontSize: "1.2vw",
-  justify: "center",
-  textAlign: "center",
-  marginTop: "auto",
-  marginBottom: "auto",
+const linkStyle = {
+  margin: "1rem",
+  textDecoration: "none",
+  color: "black",
+  fontWeight: "bold",
+  variant: "outlined",
 };
 
 class NavBar extends React.Component {
   render() {
     return (
-      <Box sx={{ flexGrow: 1 }} display="table">
-        <AppBar color="transparent">
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" color="transparent">
           <Toolbar>
             <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
-              <div className="navbartext">
-                <Stack direction="row" spacing={2}>
-                  <Link activeClass="active" to="home" spy={true} smooth={true}>
-                    <Button style={buttonStyle}>Home</Button>
-                  </Link>
-                  <Link to="tilegrid" spy={true} smooth={true} offset={-100}>
-                    <Button style={buttonStyle}> Skills</Button>
-                  </Link>
-                  <Link
-                    to="experience"
-                    spy={true}
-                    smooth={true}
-                    offset={-600}
-                    isDynamic={true}
-                  >
-                    <Button style={buttonStyle}>Experience</Button>
-                  </Link>
-                  <Link to="portfolio" spy={true} smooth={true}>
-                    <Button style={buttonStyle}>Portfolio</Button>
-                  </Link>
-                  <Link to="contact" spy={true} smooth={true}>
-                    <Button style={buttonStyle}>Contact Me</Button>
-                  </Link>
-                </Stack>
-              </div>
+              <Link to="/" style={linkStyle}>
+                Home
+              </Link>
+              <Link to="/HiddenFields" style={linkStyle}>
+                HiddenFields
+              </Link>
             </Typography>
           </Toolbar>
         </AppBar>
