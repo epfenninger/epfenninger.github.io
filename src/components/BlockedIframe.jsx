3eef2701f07eab7b5efa5168ab91fe5a67fb8ce7
epfenninger <epfenni@gmail.com>
2022-03-07 14:36:21 -0500

    iframe blocking

--- src/components/BlockedIframe.jsx
+++ src/components/BlockedIframe.jsx
@@ -6,6 +6,7 @@ import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
+import { Helmet } from "react-helmet";
 
 const defaultValues = {
   firstName: "",
@@ -46,129 +47,134 @@ const HiddenFields = () => {
   const [open, setOpen] = React.useState(false);
 
   return (
-    <form onSubmit={handleSubmit} id="myForm">
-      <Grid
-        container
-        alignItems="center"
-        justify="center"
-        direction="column"
-        rowSpacing={5}
-        position="relative"
-      >
-        <Grid item>
-          <TextField
-            id="business-email"
-            name="email"
-            label="Work Email"
-            type="email"
-            value={formValues.email}
-            onChange={handleInputChange}
-            required
-          />
+    <div>
+      <Helmet>
+        <meta http-equiv="X-Frame-Options" content="deny"></meta>
+      </Helmet>
+      <form onSubmit={handleSubmit} id="myForm">
+        <Grid
+          container
+          alignItems="center"
+          justify="center"
+          direction="column"
+          rowSpacing={5}
+          position="relative"
+        >
+          <Grid item>
+            <TextField
+              id="business-email"
+              name="email"
+              label="Work Email"
+              type="email"
+              value={formValues.email}
+              onChange={handleInputChange}
+              required
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              id="First-Name"
+              name="firstName"
+              label="First Name"
+              type="text"
+              value={formValues.firstName}
+              onChange={handleInputChange}
+              required
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              id="Last-Name"
+              name="lastName"
+              label="Last Name"
+              type="text"
+              value={formValues.lastName}
+              onChange={handleInputChange}
+              required
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              id="Company-Name"
+              name="companyName"
+              label="Company Name"
+              type="text"
+              value={formValues.companyName}
+              onChange={handleInputChange}
+              required
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              id="Job-Title"
+              name="jobTitle"
+              label="Job Title"
+              type="text"
+              value={formValues.jobTitle}
+              onChange={handleInputChange}
+              required
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              id="Phone-Number"
+              name="phoneNumber"
+              label="Phone Number"
+              type="phone"
+              value={formValues.phoneNumber}
+              onChange={handleInputChange}
+              required
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              id="message-input"
+              name="message"
+              label="Message"
+              type="text"
+              value={formValues.message}
+              onChange={handleInputChange}
+              required
+            />
+          </Grid>
+          <Grid item>
+            <Button variant="contained" color="primary" type="submit">
+              Submit
+            </Button>
+            <Dialog
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="alert-dialog-title"
+              aria-describedby="alert-dialog-description"
+            >
+              <DialogTitle id="alert-dialog-title">
+                {"Submitted Content"}
+              </DialogTitle>
+              <DialogContent>
+                {formValues.email}
+                <br></br>
+                {formValues.firstName}
+                <br></br>
+                {formValues.lastName}
+                <br></br>
+                {formValues.companyName}
+                <br></br>
+                {formValues.jobTitle}
+                <br></br>
+                {formValues.phoneNumber}
+                <br></br>
+                {formValues.message}
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose} autoFocus>
+                  Close
+                </Button>
+              </DialogActions>
+            </Dialog>
+          </Grid>
         </Grid>
-        <Grid item>
-          <TextField
-            id="First-Name"
-            name="firstName"
-            label="First Name"
-            type="text"
-            value={formValues.firstName}
-            onChange={handleInputChange}
-            required
-          />
-        </Grid>
-        <Grid item>
-          <TextField
-            id="Last-Name"
-            name="lastName"
-            label="Last Name"
-            type="text"
-            value={formValues.lastName}
-            onChange={handleInputChange}
-            required
-          />
-        </Grid>
-        <Grid item>
-          <TextField
-            id="Company-Name"
-            name="companyName"
-            label="Company Name"
-            type="text"
-            value={formValues.companyName}
-            onChange={handleInputChange}
-            required
-          />
-        </Grid>
-        <Grid item>
-          <TextField
-            id="Job-Title"
-            name="jobTitle"
-            label="Job Title"
-            type="text"
-            value={formValues.jobTitle}
-            onChange={handleInputChange}
-            required
-          />
-        </Grid>
-        <Grid item>
-          <TextField
-            id="Phone-Number"
-            name="phoneNumber"
-            label="Phone Number"
-            type="phone"
-            value={formValues.phoneNumber}
-            onChange={handleInputChange}
-            required
-          />
-        </Grid>
-        <Grid item>
-          <TextField
-            id="message-input"
-            name="message"
-            label="Message"
-            type="text"
-            value={formValues.message}
-            onChange={handleInputChange}
-            required
-          />
-        </Grid>
-        <Grid item>
-          <Button variant="contained" color="primary" type="submit">
-            Submit
-          </Button>
-          <Dialog
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="alert-dialog-title"
-            aria-describedby="alert-dialog-description"
-          >
-            <DialogTitle id="alert-dialog-title">
-              {"Submitted Content"}
-            </DialogTitle>
-            <DialogContent>
-              {formValues.email}
-              <br></br>
-              {formValues.firstName}
-              <br></br>
-              {formValues.lastName}
-              <br></br>
-              {formValues.companyName}
-              <br></br>
-              {formValues.jobTitle}
-              <br></br>
-              {formValues.phoneNumber}
-              <br></br>
-              {formValues.message}
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={handleClose} autoFocus>
-                Close
-              </Button>
-            </DialogActions>
-          </Dialog>
-        </Grid>
-      </Grid>
-    </form>
+      </form>
+    </div>
   );
 };
 export default HiddenFields;
